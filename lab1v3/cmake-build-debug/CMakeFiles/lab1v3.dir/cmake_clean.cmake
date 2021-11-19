0a9674f12ea1c9f737bfe60dfadf2bc002bdda1a
heartmlnd <91538319+heartmlnd@users.noreply.github.com>
2021-11-19 15:59:55 +0300

    Add files via upload

---
+++ lab1v3/cmake-build-debug/CMakeFiles/lab1v3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1v3.dir/main.cpp.obj"
+  "lab1v3.exe"
+  "lab1v3.exe.manifest"
+  "lab1v3.pdb"
+  "liblab1v3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1v3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
